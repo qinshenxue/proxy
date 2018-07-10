34694c9f530308f16cdc7354401f8105529505d6
qinshenxue <qinshenxue@qq.com>
2018-07-10 16:47:14 +0800

    add

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express = require('express')
 const app = express()
 
 
+app.use('*',function (req,res) {
+	// body...
+
+	console.log(req.path)
+	res.send({
+		success:1
+	})
+})
+
 
 app.listen(443, function() {
     console.log('proxy start')
