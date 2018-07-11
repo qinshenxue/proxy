32433867353293c27aeb21f97b6267e2d66f2e4b
qinshenxue <qinshenxue@qq.com>
2018-07-11 08:05:21 +0800

    hook

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const express = require('express')
 
 const app = express()
 
+app.use('*', function(req, res) {
+    // body...
 
-app.use('*',function (req,res) {
-	// body...
-
-	console.log(req.path)
-	res.send({
-		success:1
-	})
+    console.log(req.path)
+    res.send({
+        success: 1
+    })
 })
-
 
 app.listen(443, function() {
     console.log('proxy start')
